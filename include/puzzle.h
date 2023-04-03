4d5af10c5e04df7a6983c37944288ba8daec9625
Leonardo Nerone <leofnerone@proton.me>
2023-04-03 11:50:01 -0400

    add init and clear for solution and remove test/ for now

--- include/puzzle.h
+++ include/puzzle.h
@@ -7,18 +7,22 @@
 #define __LHP_PUZZLE__
 
 typedef struct LHP_puzzle_t {
-	mpz_t u ;
-	mpz_t v ;
-} LHP_puzzle_t ;
+  mpz_t u;
+  mpz_t v;
+} LHP_puzzle_t;
 
 typedef struct LHP_puzzle_sol_t {
-	mpz_t s ;
-} LHP_puzzle_sol_t ;
+  mpz_t s;
+} LHP_puzzle_sol_t;
 
-void LHP_init_puzzle ( LHP_puzzle_t* puzzle ) ;
+void LHP_init_puzzle(LHP_puzzle_t *puzzle);
 
-void LHP_clear_puzzle ( LHP_puzzle_t* puzzle ) ;
+void LHP_clear_puzzle(LHP_puzzle_t *puzzle);
 
-int LHP_puzzle_cmp ( LHP_puzzle_t* a , LHP_puzzle_t* b ) ;
+int LHP_puzzle_cmp(LHP_puzzle_t *a, LHP_puzzle_t *b);
+
+void LHP_init_solution(LHP_puzzle_sol_t *sol);
+
+void LHP_clear_solution(LHP_puzzle_sol_t *sol);
 
 #endif
--- src/puzzle.c
+++ src/puzzle.c
@@ -7,24 +7,24 @@
 
 #include "puzzle.h"
 
-void LHP_init_puzzle ( LHP_puzzle_t* puzzle )
-{
-	mpz_init ( puzzle->u ) ;
-	mpz_init ( puzzle->v ) ;
+void LHP_init_puzzle(LHP_puzzle_t *puzzle) {
+  mpz_init(puzzle->u);
+  mpz_init(puzzle->v);
 }
 
-void LHP_clear_puzzle ( LHP_puzzle_t* puzzle )
-{
-	mpz_clear ( puzzle->u ) ;
-	mpz_clear ( puzzle->v ) ;
+void LHP_clear_puzzle(LHP_puzzle_t *puzzle) {
+  mpz_clear(puzzle->u);
+  mpz_clear(puzzle->v);
 }
 
-int LHP_puzzle_cmp ( LHP_puzzle_t* a , LHP_puzzle_t* b )
-{
-	int status = 0 ;
-	if ( ( status = mpz_cmp ( a -> u , b -> u ) ) != 0 ) {
-		return status ;
-	}
-	return mpz_cmp ( a -> v , b -> v ) ;
+int LHP_puzzle_cmp(LHP_puzzle_t *a, LHP_puzzle_t *b) {
+  int status = 0;
+  if ((status = mpz_cmp(a->u, b->u)) != 0) {
+    return status;
+  }
+  return mpz_cmp(a->v, b->v);
 }
 
+void LHP_init_solution(LHP_puzzle_sol_t *solution) { mpz_init(solution->s); }
+
+void LHP_clear_solution(LHP_puzzle_sol_t *solution) { mpz_clear(solution->s); }
